0891fd838fb57b6d6acf52a1a422661e0a0aa628
Franco Miguez <franco.miguez98@gmail.com>
2023-04-12 16:27:27 -0300

    init project section

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import NavBar from './components/NavBar'
 import Skills from "./components/Skills";
 import Header from './components/Header'
+import Projects from './components/Projects'
 
 function App() {
     return <>
         <NavBar/>
         <Header/>
+        <Projects/>
         <Skills/>
     </>;
 }
---
+++ portafolio/src/components/Projects.jsx
@@ -0,0 +1,10 @@
+import TitleSection from './TitleSection'
+
+function Projects() {
+  return (
+    <section id='projects' className="min-h-screen pt-20 bg-gray-950 flex flex-col items-center">
+      <TitleSection text="Projects"/>
+    </section>
+  )
+}
+export default Projects
