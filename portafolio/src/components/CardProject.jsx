2b43721ee909ad1e07f988c5903aec43ab63a8f9
Franco Miguez <franco.miguez98@gmail.com>
2023-04-15 15:01:22 -0300

    end create layout card

--- portafolio/src/components/CardProject.jsx
+++ portafolio/src/components/CardProject.jsx
@@ -1,17 +1,68 @@
-function CardProject({img, title, description, languages, linkGithub, linkPage}) {
+import {useState} from 'react'
+import {BiLink} from 'react-icons/bi'
+import {AiFillGithub, AiFillHtml5} from 'react-icons/ai'
+import {
+    SiCss3,
+    SiJavascript,
+    SiPython,
+    SiJson,
+    SiTailwindcss,
+} from "react-icons/si";
+import { DiReact } from "react-icons/di";
+import { BsBootstrapFill } from "react-icons/bs";
+
+function CardProject({img, title, description, languages, links, id, create}) {
+
+  const [clickShowMore, setClickShowMore] = useState(false)
+
+  const showMore = (text)=>{
+    if(text.length > 100 && !clickShowMore){
+      return `${text.substr(0,100)}...`
+    }else return text
+  }
+
+  const languagesIcon = (name)=>{
+    switch(name){
+      case "Python": return <SiPython/>
+      case "Javascript": return <SiJavascript/>
+      case "CSS": return <SiCss3/>
+      case "Json": return <SiJson/>
+      case "Tailwindcss": return <SiTailwindcss/>
+      case "HTML": return <AiFillHtml5/>
+      case "React": return <DiReact/>
+      case "Bootstrap": return <BsBootstrapFill/>
+    }
+  }
+
+
+
   return (
-    <div className="bg-gray-900 p-5 h-96 w-3/4 sm:w-64 rounded-xl shadow-md hover:shadow-lg hover:shadow-cyan-600 shadow-red-500 flex flex-col items-center transition-all duration-500">
-        <header>
-            <img className="rounded-2xl shadow-md shadow-gray-950" src={img} alt="" />
-            <h3>{title}</h3>
+    <div id={id} className={`bg-gray-900  w-3/4  rounded-xl shadow-md hover:shadow-lg hover:shadow-cyan-600 shadow-red-500 flex flex-col items-center transition-transform duration-500 scale-95 hover:scale-100 ${clickShowMore? "sm:w-full" :"sm:w-64"}`}>
+        <header className="mx-3 justify-items-start my-3">
+            <img className={`rounded-2xl shadow-lg shadow-gray-950 object-cover h-40 ${clickShowMore?"sm:h-80":""}`} src={img} alt="" />
+            <h3 className="text-center font-bold py-3">{title}</h3>
         </header>
-        <section>
-            {description}
+        <section className="mx-3 flex flex-col">
+            <p className='text-gray-500 self-end'>{create}</p>
+            {showMore(description)}
+            <a href={"/#" + id} onClick={()=>setClickShowMore(!clickShowMore)} className={description.length > 100?'bg-cyan-500 my-2 rounded-lg py-1 hover:shadow-sm hover:shadow-cyan-300 hover:bg-cyan-800 text-center':'hidden'}>
+              {clickShowMore?"Show Less": "Show More"}
+            </a>
         </section>
-        <footer>
-            {languages}
-            {linkGithub}
-            {linkPage}
+        <footer className="bg-gray-950  rounded-b-xl w-full">
+            <div className="py-2 border-b-2 border-white text-3xl flex justify-center gap-5">
+              {languages.map((value)=> languagesIcon(value))}
+            </div>
+            <div className="py-2">
+              <ul className='flex justify-center text-5xl'>
+                <li className={links.github?"" : "hidden"}>
+                  <a target='_black' href={links.github}><AiFillGithub/></a>
+                </li>
+                <li className={links.page?"" : "hidden"}>
+                  <a target='_black' href={links.page}><BiLink/></a>
+                </li>
+              </ul>
+            </div>
         </footer>
     </div>
   )
--- portafolio/src/components/Projects.jsx
+++ portafolio/src/components/Projects.jsx
@@ -1,18 +1,36 @@
+import {useState, useEffect} from 'react'
 import TitleSection from './TitleSection'
 import CardProject from './CardProject'
 
 function Projects() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("./data.json")
+      const json = await response.json()
+      setData(json.projects.sort((a,b)=>{
+        return a.create > b.create ? -1 : 1
+      }))
+  }
+  fetchData()
+  },[])
+
   return (
-    <article id='projects' className="min-h-screen pt-20 bg-gray-950 flex flex-col items-center">
+    <article id='projects' className="min-h-screen py-20 bg-gray-950 flex flex-col items-center ">
       <TitleSection text="Projects"/>
-      <CardProject
-      title="Titulo"
-      img="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-      description="sdasdasdasdas asdasdasdasdasdsad adasdasda sdasdasdasdasdasdasdasd"
-      languages={["1","2"]}
-      linkGithub="https://github.com/"
-      linkPage="franco-miguez.com"
-      />
+      <div className='flex mt-10 flex-wrap gap-10 justify-center'>
+        {data.map((project)=><CardProject
+        key={project.id}
+        id={project.id}
+        title={project.title}
+        img={project.image}
+        create={project.create}
+        description={project.description}
+        languages={project.languages}
+        links={project.links}
+        />)}
+      </div>
     </article>
   )
 }
