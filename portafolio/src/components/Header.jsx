876d921ce656238ddcc7db4cc9d3d9ee38ec53ae
Franco Miguez <franco.miguez98@gmail.com>
2023-04-17 21:33:07 -0300

    add new photo and remove image the project in assets

--- portafolio/src/components/Header.jsx
+++ portafolio/src/components/Header.jsx
@@ -6,8 +6,8 @@ function Header() {
     return (
     <header id="i" className="min-h-screen pt-20 bg-gray-900 flex flex-col items-center">
         <div className='relative w-3/4 md:w-2/5  bg-black rounded-full shadow-lg shadow-cyan-500'>
-            <img className='absolute' src={iconImage} alt="" />
-            <img src={protraitImage} alt="frontal photo" className='rounded-full  hover:opacity-0  transition-all duration-700 relative'/>
+            <img className='absolute w-full' src={iconImage} alt="" />
+            <img src={protraitImage} alt="frontal photo" className='rounded-full  hover:opacity-0  transition-all duration-700 relative w-full'/>
         </div>
         <div className='my-10 lg:my-20 bg-gray-950 p-5 lg:p-10 rounded-2xl shadow-lg shadow-cyan-500'>
             <h1 className="font-bold text-sky-300 text-3xl text-center md:text-5xl pt-5">Franco Miguez</h1>
